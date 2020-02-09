b943b9c6b1ccacae4688a8ef8ec5603ee2d0202a
caozilu <caozilu@outlook.com>
2020-02-09 14:22:09 +0800

    add server controller for CORS

--- src/main/java/com/microsoft/com/demo/model/ContactRepository.java
+++ src/main/java/com/microsoft/com/demo/model/ContactRepository.java
@@ -1,6 +1,8 @@
 package com.microsoft.com.demo.model;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ContactRepository extends CrudRepository<Contact, Long> {
-    
+
 }
